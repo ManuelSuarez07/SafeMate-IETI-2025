e672ae3fff4325a785407821ef904d28d28625eb
ManuelSuarez07 <Manuel.suarez.e09@gmail.com>
2025-11-28 23:24:17 -0500

    Correcciones V2.2.3

--- savemate-mobile/android/build.gradle.kts
+++ savemate-mobile/android/build.gradle.kts
@@ -1,3 +1,10 @@
+plugins {
+    // 1. Declaración del Android Gradle Plugin
+    id("com.android.application") version "8.7.3" apply false
+    // 2. Declaración del Kotlin Android Plugin (SOLUCIÓN al error 'plugin not found')
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+}
+
 allprojects {
     repositories {
         google()
